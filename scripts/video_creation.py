6c8ce16807bb5511bea8f98575600f21f193c80c
Abdul Rahman Quraishi <abdul.quraishi@10xengineers.ai>
2023-11-27 18:31:42 +0500

    updated video_creation.py

--- scripts/video_creation.py
+++ scripts/video_creation.py
@@ -12,16 +12,21 @@ import numpy as np
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from datetime import datetime
+from pathlib import Path
 import cv2
 import os
 
-filepath = './ov5647/Burst_Capture/'
-dest = './ov5647/Video_Frames/'
-h, w =  1080, 1920
-stride = np.floor(np.floor(np.floor((w*3 + 255) /256)) * 256)
-stride = stride.astype(np.uint16)
-img = np.zeros((h,w,3), dtype=np.uint8)
+# Path of the directory containing "OV5467" and "AR1335" directories
+path =  "./ISP_Output"
 
+# Supported Sensors and selected sensor (SENSOR)
+SupportedSensors = {
+    "AR1335": 1,
+    "OV5647": 2 
+}
+SENSOR = "OV5647"
+
+# Supported ISP output formats
 Supported_Formats = {
     "RGB" : 1,
     "YUV" : 2
@@ -30,12 +35,32 @@ Supported_Formats = {
 # Select the format by copying supported format string from above
 Selected_Format = "YUV"
 
-start_index = 1
-end_index = 250
+# Starting and ending frames of the video sequence
+start_index, end_index = 1, 250
+
+# Size of the output image
+h, w =  1080, 1920
+
+# parent directory
+p = Path(path)
+parent_dir = p.resolve().joinpath(SENSOR)
+
+# Making directory for saving .png files
+bin_dir = "Burst_Capture"       # Created by firmware, DO NOT MODIFY
+bin_path = parent_dir / bin_dir
+video_dir = "Video_Frames"
+video_path = parent_dir/ video_dir
+Path.mkdir(video_path,exist_ok=True) 
+
+# Images are stored in the form of rows where the size of each row in bytes
+# should be a multiple of 256, each such row size is called 'stride'
+stride = np.floor(np.floor(np.floor((w*3 + 255) /256)) * 256)
+stride = stride.astype(np.uint16)
+img = np.zeros((h,w,3), dtype=np.uint8)
 
 for m in range(start_index,end_index+1):
     # repalce the first number in the filename as per the iteration number 
-    filename =filepath + Selected_Format + str(m) + '.bin'
+    filename =str(bin_path) + '/' + Selected_Format + str(m) + '.bin'
     with open(filename, 'rb') as f:
         arr = np.fromfile(f, dtype=np.uint8)
     f.close()
@@ -56,23 +81,36 @@ for m in range(start_index,end_index+1):
     G = G_flat.reshape(h,w)
     B = B_flat.reshape(h,w)
 
+    if(Supported_Formats[Selected_Format] == Supported_Formats["RGB"]):
+        # In case of RGB, switch the 1st and the 3rd channels
+        img[:,:,0] = B
+        img[:,:,1] = G
+        img[:,:,2] = R
+    else:
+        # In case of YUV, don't switch the 1st and the 3rd channels
+        img[:,:,0] = R
+        img[:,:,1] = G
+        img[:,:,2] = B
 
-    img[:,:,0] = B
-    img[:,:,1] = G
-    img[:,:,2] = R
+        # Converting YUV frame to RGB frame
+        img = cv2.cvtColor(img, cv2.COLOR_YUV2BGR)
 
-    plt.imsave(dest + str(m) +'.png', img[:,:,:])
+    plt.imsave(str(video_path) + '/' + str(m) +'.png', img[:,:,:])
     print(str(m)+'/'+str(end_index))
 
 
 # Path to the folder containing the PNG images
-image_folder = dest
+image_folder = str(video_path)
 
 # Output video file name
 video_name = 'Video.mp4'
 
 # Frame rate of the output video
-fps = 30
+if(SupportedSensors[SENSOR] == SupportedSensors['AR1335']):
+    fps = 30
+
+if(SupportedSensors[SENSOR] == SupportedSensors['OV5647']):
+    fps = 15
 
 # Function to sort the images numerically
 def sort_key(s):
@@ -88,7 +126,7 @@ height, width, layers = frame.shape
 
 # Create a VideoWriter object to save the video
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-video = cv2.VideoWriter(video_name, fourcc, fps, (width, height))
+video = cv2.VideoWriter(image_folder + '/' + video_name, fourcc, fps, (width, height))
 
 # Iterate over the images and write each frame to the video
 for image in images:
